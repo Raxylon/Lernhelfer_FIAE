bf1b1dcdaaa63893a9e75b274665086e1a56a71c
Raxylon <maikmrowetz@gmail.com>
2025-12-02 22:42:20 +0100

    absoluten pfad für json.load eingebaut

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import json
+import os, json
 
-with open("lernmodule.json", "r", encoding="utf-8") as f:
+BASE = os.path.dirname(os.path.abspath(__file__))
+JSON_PATH = os.path.join(BASE, "lernmodule.json")
+
+with open(JSON_PATH, "r", encoding="utf-8") as f:
     daten = json.load(f)
 while True:
     try:
